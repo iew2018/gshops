c4026faebb5dd55f66a123c07187e3acbf662463
陈__某 <2201330789@qq.com>
2021-05-14 19:02:55 +0800

    init gshops

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide';
+  export default {
+    components: {
+      FooterGuide,
+    },
+  };
+</script>
+<style lang='stylus'></style>
